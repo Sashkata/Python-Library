ce71e912786d7ffd1dd922ae27924c69bb5226b0
Aleks <alekspenev347@gmail.com>
2019-06-13 21:09:00 -0500

    Main program

---
+++ main.py
@@ -0,0 +1,50 @@
+import tkinter
+from pathlib import Path
+import os
+
+currentLibrary = []
+currentLocation = Path("./Library")
+mainWindow = tkinter.Tk()
+mainWindow.geometry("400x795+1120+0")
+
+
+def run_Library():
+    global currentLocation
+    tkinter.Button(mainWindow, text="Back",fg ="white",bg="grey", width = 30, command=lambda: goBack(currentLocation)).pack(pady=10)
+    populate(currentLocation)
+    mainWindow.mainloop()
+
+def populate(loc):
+    currentLibrary.clear()
+    for item in loc.iterdir():
+        currentLibrary.append(item)
+    createButtons()
+
+def createButtons():
+    for item in currentLibrary:
+        tkinter.Button(mainWindow, text=item.stem, width=70, command=lambda item=item:fileHandler(item)).pack(pady=3)
+
+def fileHandler(inpt):
+    global currentLocation
+    if inpt.is_dir():
+        clearFrame()
+        currentLocation = inpt
+        populate(currentLocation)
+    else:
+        os.startfile(inpt)
+
+def goBack(loc):
+    global currentLocation
+    if not currentLocation.name == "Library":
+        clearFrame()
+        currentLocation = loc.parent
+        populate(loc.parent)
+
+
+def clearFrame():
+    optionList = mainWindow.pack_slaves()
+    for btn in optionList:
+        if not btn["text"]=="Back":
+            btn.destroy();
+
+run_Library()
